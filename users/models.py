0aba996ddb97b63d2f5f0f497db18d3bda2ca6e4
livingmugash <mugash042@gmail.com>
2025-11-27 13:00:37 +0300

    Update models.py

--- users/models.py
+++ users/models.py
@@ -5,6 +5,18 @@ class User(AbstractUser):
     email = models.EmailField(unique=True)
     github_access_token = models.CharField(max_length=255, blank=True, null=True)
     
+    # Resolves conflict with Django's default User model
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='applaude_user_set',
+        blank=True
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='applaude_user_set',
+        blank=True
+    )
+    
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
 
