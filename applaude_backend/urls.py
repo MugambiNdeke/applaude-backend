f888e7c300db08ce051e1bd71942b63d809b0d57
livingmugash <mugash042@gmail.com>
2025-11-27 15:22:49 +0300

    Update urls.py

--- applaude_backend/urls.py
+++ applaude_backend/urls.py
@@ -9,6 +9,7 @@ router.register(r'tests', TestRunViewSet, basename='testrun')
 urlpatterns = [
     path('admin/', admin.site.urls),
     # The fix: Ensure both quotes and parenthesis are present
+    path('api/auth/', include('users.urls')),
     path('api/auth/', include('users.urls')), 
     path('api/payments/', include('payments.urls')),
     path('api/', include(router.urls)),
