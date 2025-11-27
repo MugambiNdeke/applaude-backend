ea87bef906f8e1c09076ac424dfed31ec0626467
livingmugash <mugash042@gmail.com>
2025-11-27 12:26:08 +0300

    Update urls.py

--- applaude_backend/urls.py
+++ applaude_backend/urls.py
@@ -1,1 +1,14 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from core.views import TestRunViewSet
 
+router = DefaultRouter()
+router.register(r'tests', TestRunViewSet, basename='testrun')
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/auth/', include('users.urls')),
+    path('api/payments/', include('payments.urls')),
+    path('api/', include(router.urls)),
+]
