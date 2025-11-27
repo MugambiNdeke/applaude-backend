0d9a082349d0e598754bbabe91b1cd8867efaea4
livingmugash <mugash042@gmail.com>
2025-11-27 12:20:42 +0300

    Update celery.py

--- applaude_backend/celery.py
+++ applaude_backend/celery.py
@@ -1,1 +1,7 @@
+import os
+from celery import Celery
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'applaude_backend.settings')
+app = Celery('applaude_backend')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
