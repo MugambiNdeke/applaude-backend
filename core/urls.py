d0f066286f14deb6bf638101e80ae224e55a777a
livingmugash <mugash042@gmail.com>
2025-11-27 13:04:21 +0300

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,1 +1,10 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import TestRunViewSet
 
+router = DefaultRouter()
+router.register(r'tests', TestRunViewSet, basename='testrun')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
