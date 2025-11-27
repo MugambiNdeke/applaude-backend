b98863e91e4e97365f57afd45d544abda7092c4d
livingmugash <mugash042@gmail.com>
2025-11-27 14:39:45 +0300

    Update wsgi.py

--- applaude_backend/wsgi.py
+++ applaude_backend/wsgi.py
@@ -1,1 +1,8 @@
+import os
+from django.core.wsgi import get_wsgi_application
 
+# Point to your project's settings module
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'applaude_backend.settings')
+
+# This is the entry point Gunicorn looks for
+application = get_wsgi_application()
