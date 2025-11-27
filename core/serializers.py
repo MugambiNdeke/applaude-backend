77e4e031d70141835513a920cb4e4b7d6649e140
livingmugash <mugash042@gmail.com>
2025-11-27 12:59:31 +0300

    Update serializers.py

--- core/serializers.py
+++ core/serializers.py
@@ -1,1 +1,8 @@
+from rest_framework import serializers
+from .models import TestRun
 
+class TestRunSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TestRun
+        fields = '__all__'
+        read_only_fields = ('user', 'status', 'result_json', 'created_at', 'progress')
